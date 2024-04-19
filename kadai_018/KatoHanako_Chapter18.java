490dfe44ac5a02331f8f54354f6091f681018482
samu-koi310 <167385880+samu-koi310@users.noreply.github.com>
2024-04-19 13:50:54 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,16 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+
+	void setGivenName() {
+		givenName = "花子";
+		System.out.println("名前は" + familyName + givenName + "です");
+	}
+	
+	public void eachIntroduce() {
+			System.out.println("趣味は読書です");
+			
+		}
+		
+		
+}
---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+	
+	void setGivenName() {
+		givenName = "一郎";
+		System.out.println("名前は" + familyName + givenName + "です");
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+		
+	}
+	
+}
